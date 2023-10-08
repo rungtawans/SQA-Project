0e561550eefbdb612351f24d0ea18f1a05c433f1
“Rungtawan <rungtawan.s@kkumail.com>
2023-10-09 03:03:27 +0700

    edit UserFactory , phpunit head and delete exampleTest

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,52 +1,3 @@
-<<<<<<< HEAD
-<?php
-
-namespace Database\Factories;
-
-use App\Models\User;
-use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
-
-class UserFactory extends Factory
-{
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = User::class;
-
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
-    public function definition()
-        {
-            return [
-                'email' => $this->faker->unique()->safeEmail,
-                'email_verified_at' => now(),
-                'fname' => 'Rungtawan',
-                'lname' =>'Suttho'
-            ];
-        }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return \Illuminate\Database\Eloquent\Factories\Factory
-     */
-    public function unverified()
-    {
-        return $this->state(function (array $attributes) {
-            return [
-                'email_verified_at' => null,
-            ];
-        });
-    }
-}
-||||||| empty tree
-=======
 <?php
 
 namespace Database\Factories;
@@ -103,4 +54,4 @@ class UserFactory extends Factory
         return $this->role === $role; // Assuming 'role' is the column storing role identifiers in your "users" table.
     }
 }
->>>>>>> 45294aaafd0296dad1bed214d1cb0114d25df1b0
+
